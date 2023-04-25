704abb080673f1072daabb2c6dc450e4644fbabd
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-04-25 10:56:07 +0530

    Kth Smallest Element in a BST

---
+++ C++/loops.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int index = 0;
+    /*while (index<=10)
+    {
+        cout<<"the index is"<<index<<endl;
+        index = index+1;
+
+    }*/
+    /*do
+    {
+        cout << "the index is" << endl
+             << index << endl;
+        index = index + 1;
+    } while (index <= 10);*/
+    for ( index = 0; index <=10; index++)
+    {
+        cout<<"the index is"<<endl<<index<<endl;
+        
+    }
+    
+
+    return 0;
+}
